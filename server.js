13de12e30bd625b1f952677ac138729862894da1
Cesar Gonzalez <jugador409@hotmail.com>
2022-05-14 01:42:57 -0500

    Se agrega crud para profiles

--- server.js
+++ server.js
@@ -11,6 +11,49 @@ app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
 
+app.get('/profiles', async (req, res) => {
+    const allProfiles =  await prisma.profile.findMany({});
+    res.json(allProfiles);
+  });
+
+  app.get('/profiles/:id', async (req, res) => {
+    const id = req.params.id;
+    const profile = await prisma.profile.findUnique({where: {id: parseInt(id)}});
+    res.json(profile);
+  });
+  
+  app.post('/profiles', async (req, res) => {
+    const profile = {
+      description: req.body.description,
+      amount: req.body.amount
+     };
+    const message = 'Perfil creado.';
+    await prisma.profile.create({data: profile});
+    return res.json({message});
+  });
+  
+  app.put('/profiles/:id', async (req, res) => {
+      const id = parseInt(req.params.id);
+  
+      await prisma.profile.update({
+          where: {
+              id: id
+          },
+          data: {
+            description: req.body.description,
+            amount: req.body.amount
+          }
+      })
+  
+      return res.json({message: "Actualizado correctamente"});
+  });
+  
+  app.delete('/profiles/:id', async (req, res) => {
+      const id = parseInt(req.params.id);
+      await prisma.profile.delete({where: {id: id}});
+      return res.json({message: "Eliminado correctamente"});
+  });
+  
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
