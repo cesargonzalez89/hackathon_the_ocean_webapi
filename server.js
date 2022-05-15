f4a25dd31fffe868c2e9df6cd560bde08ad34bc4
Cesar Gonzalez <jugador409@hotmail.com>
2022-05-15 18:15:21 -0500

    Se agrega configuracion de cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 console.log(Conn.getConnection);
 
 var corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:8080',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
